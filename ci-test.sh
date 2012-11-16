9d9f55e49ce61e23800bf943a3f6248bc732afdd
Michael Schleichardt <michael@schleichardt.net>
2012-11-16 13:12:27 +0100

    disable color logging for ci tests, close #2

--- ci-test.sh
+++ ci-test.sh
@@ -1,5 +1,5 @@
 set -x
 
-export xsbt="$(pwd)/sbt"
+export xsbt="$(pwd)/sbt -Dsbt.log.noformat=true"
 chmod a+x sbt sbtwrapper/sbt-launch.jar
 cd sbt-plugin && $xsbt publish-local && cd ../plugin && $xsbt publish-local && cd ../example && $xsbt test
