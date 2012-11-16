4e51b20c94eab576bac1ddf2707b375af2d974ea
Michael Schleichardt <michael@scheichardt.net>
2012-11-16 09:34:31 +0100

    added empty plugin project #1

---
+++ plugin/project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "play-coffee-qunit"
+    val appVersion      = "0.1-SNAPSHOT"
+    lazy val publishingFolder = Path.userHome.absolutePath+"/Projekte/schleichardt.github.com/jvmrepo"
+
+    val appDependencies = Seq(
+      // Add your project dependencies here,
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies).settings(
+      organization := "info.schleichardt",
+      publishTo := Some(Resolver.file("file",  new File(publishingFolder))(Resolver.mavenStylePatterns)),
+      publishMavenStyle := true 
+    )
+
+}
