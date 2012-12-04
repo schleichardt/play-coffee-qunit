656aa31e00e6ccfbeea6be97117db99d44439084
Michael Schleichardt <michael@schleichardt.net>
2012-12-04 20:06:23 +0100

    make qunit.js and qunit.css available via /@qunit?asset=qunit.js, not asset folder, close #10

--- plugin/app/controllers/QUnit.scala
+++ plugin/app/controllers/QUnit.scala
@@ -6,12 +6,14 @@ import io.Source
 
 object QUnit extends Controller {
 
-  def index(htmlFile: String) = Action {
+  def index(htmlFile: String, asset: String) = Action { request =>
     if(htmlFile != "") {
       val source = scala.io.Source.fromFile(htmlFile, "utf-8")
       val htmlContent = source .mkString
       source.close()
       Ok(htmlContent).as("text/html")
+    } else if (asset != "") {
+      controllers.Assets.at(path="/public", file=asset)(request)
     } else {
       Ok("needle")
     }
