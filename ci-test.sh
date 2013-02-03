fdb8e63f49845152edb27bc7a6a9770adbc2a35a
Michael Schleichardt <michael@schleichardt.net>
2013-02-04 00:19:56 +0100

    call test for plugin and sbt-plugin in ci script

--- ci-test.sh
+++ ci-test.sh
@@ -2,4 +2,4 @@ set -x
 
 export xsbt="$(pwd)/sbt -Dsbt.log.noformat=true"
 chmod a+x sbt sbtwrapper/sbt-launch.jar
-cd sbt-plugin && $xsbt clean publish-local && cd ../plugin && $xsbt clean publish-local && cd ../testProject && $xsbt test
+cd sbt-plugin && $xsbt clean test publish-local && cd ../plugin && $xsbt clean test publish-local && cd ../testProject && $xsbt test
