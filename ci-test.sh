b77f04d159c6e0e646e897913b6fda05efeadb40
Michael Schleichardt <michael@schleichardt.net>
2013-02-03 20:55:50 +0100

    renamed example to testProject since it is no demo application

--- ci-test.sh
+++ ci-test.sh
@@ -2,4 +2,4 @@ set -x
 
 export xsbt="$(pwd)/sbt -Dsbt.log.noformat=true"
 chmod a+x sbt sbtwrapper/sbt-launch.jar
-cd sbt-plugin && $xsbt publish-local && cd ../plugin && $xsbt publish-local && cd ../example && $xsbt test
+cd sbt-plugin && $xsbt clean publish-local && cd ../plugin && $xsbt clean publish-local && cd ../testProject && $xsbt test
