99ffd6c4ade1ad026cf1c587f5ec5831689d005c
Michael Schleichardt <michael@schleichardt.net>
2013-02-23 09:57:21 +0100

    update test for play bug

--- plugin/test/QUnitTestRunnerSpec.scala
+++ plugin/test/QUnitTestRunnerSpec.scala
@@ -5,7 +5,7 @@ class QUnitTestRunnerSpec extends Specification {
 
   "The QUnitTestsRunner" should {
     "be able to create the url from a template name to the test with it" in {
-      QUnitTestsRunner.testFileToUrlPath("views.html.test.sub1.sub2.test1") === "/html/views.html.test.sub1.sub2.test1"
+      QUnitTestsRunner.testFileToUrlPath("views.html.test.sub1.sub2.test1") === "//html/views.html.test.sub1.sub2.test1" //included workaround for play bug
     }
   }
 }
