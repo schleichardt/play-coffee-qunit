8e099811ff2f4d2bf8f942423512a7961416ed6f
Michael Schleichardt <michael@schleichardt.net>
2013-04-11 20:51:39 +0200

    disable QUnit actions in prod

--- plugin/app/controllers/qunit/QUnit.scala
+++ plugin/app/controllers/qunit/QUnit.scala
@@ -1,41 +1,42 @@
 package controllers.qunit
-
 import play.api.mvc._
 import play.api._
-import http.MimeTypes
 import play.api.templates._
 import qunit.QUnitUtils._
 import ro.isdc.wro.extensions.processor.js.RhinoCoffeeScriptProcessor
 import java.io.{FileReader, StringWriter}
-import MimeTypes.JAVASCRIPT
+import play.api.Play.current
 
 object QUnit extends Controller {
-
   private type TemplateMapping = Map[String, Template0[Result]]
 
+  def NonProdAction(action: Request[AnyContent] => Result): Action[AnyContent] = {
+    Action { request => if (Play.isProd) NotFound else action(request) }
+  }
+
   private lazy val testTemplateNameToClassMap: TemplateMapping = {
-    val clazz = Play.current.classloader.loadClass("controllers.qunit.QUnitTemplateMapping") //Class.forName() does not work here
+    val clazz = Play.current.classloader.loadClass("controllers.qunit.QUnitTemplateMapping")
     val instance = clazz.getConstructor().newInstance()
     val getter = clazz.getMethod("testTemplateNameToClassMap")
     getter.invoke(instance).asInstanceOf[TemplateMapping]
   }
 
-  def index = Action { implicit request =>
+  def index = NonProdAction { implicit request =>
     Ok(views.html.qunit.index(classUrlPathList, classNameList))
   }
 
-  def html(file: String) = Action { implicit request =>
+  def html(file: String) = NonProdAction { implicit request =>
     testTemplateNameToClassMap.get(file) match {
       case Some(r: Template0[_]) => Ok(r.render().asInstanceOf[Html])
       case None => NotFound("No test found for " + file)
     }
   }
 
-  def javascript(file: String) = Action { implicit request =>
+  def javascript(file: String) = NonProdAction { implicit request =>
     Ok.sendFile(Play.current.getFile("test/" + file)).as(JAVASCRIPT)
   }
 
-  def csAsJs(file: String) = Action { implicit request =>
+  def csAsJs(file: String) = NonProdAction { implicit request =>
     val input = new FileReader(Play.current.getFile("test/" + file))
     val writer = new StringWriter()
     (new RhinoCoffeeScriptProcessor).process(input, writer)
