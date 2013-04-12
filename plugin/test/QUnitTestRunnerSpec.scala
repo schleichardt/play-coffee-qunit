358de0d1afd652b209d15f84f0408bb76562ea6b
Michael Schleichardt <michael@schleichardt.net>
2013-04-12 17:14:56 +0200

    routes order

--- plugin/test/QUnitTestRunnerSpec.scala
+++ plugin/test/QUnitTestRunnerSpec.scala
@@ -5,7 +5,7 @@ class QUnitTestRunnerSpec extends Specification {
 
   "The QUnitTestsRunner" should {
     "be able to create the url from a template name to the test with it" in {
-      QUnitUtils.testFileToUrlPath("views.html.test.sub1.sub2.test1") === "/html/views.html.test.sub1.sub2.test1"
+      QUnitUtils.testFileToUrlPath("views.html.test.sub1.sub2.test1") === "//html/views.html.test.sub1.sub2.test1"
     }
   }
 }
